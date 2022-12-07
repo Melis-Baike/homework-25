7ed6800184db8e2963e9a0a13bab59c6aae5dd69
Erkin Imakeev <erkinimakeev7@gmail.com>
2022-12-07 15:27:21 +0600

    Added methods for choosing a value for a person.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,38 @@ public class Main {
         Computer computer = new Computer();
         Human human = new Human();
         selectMeaningToComputer(computer);
+        chooseMeaningToHuman(human);
+    }
+
+    public static void chooseMeaningToHuman(Human human){
+        System.out.println("1 - Rock\n2 - Paper\n3 - Scissors");
+        System.out.print("Enter the value you want to select : ");
+        try {
+            int meaning = Integer.parseInt(sc.next());
+            checkOutOfRange(meaning);
+            takeChosenMeaning(human, meaning);
+        } catch (NumberFormatException e){
+            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void checkOutOfRange(int number) throws Exception{
+        if(number < 1 || number > 3){
+            throw new OutOfRangeException("You are out of range");
+        }
+    }
+
+
+    public static void takeChosenMeaning(Human human, int meaning){
+        switch (meaning) {
+            case 1 : human.meaning = Meanings.ROCK;
+                break;
+            case 2 : human.meaning = Meanings.PAPER;
+                break;
+            case 3 : human.meaning = Meanings.SCISSORS;
+        }
     }
 
     public static void selectMeaningToComputer(Computer computer){
